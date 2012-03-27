d843d3205e722e109e3a2b059648e36d00a90e1f
JuKu <JustinKuenzel@aol.com>
2012-03-27 14:13:50 +0200

    Kommentar schreiben - Formular hinzugefügt.

--- styles/default/comments.php
+++ styles/default/comments.php
@@ -5,4 +5,40 @@
                 <td>{COMMENTS}</td>
             </tr>
         </table>
+        <br />
+        
+        <b>Kommentar schreiben</b>
+        
+        <form action="{PAGE}" method="post">
+            <input type="hidden" name="userid" value="{USERID}" />
+            <div class="comment_form">
+                <table border="0">
+                    <tr>
+                        <td width="200">Name: </td>
+                        <td width="200"><input type="text" name="name" /></td>
+                    </tr>
+                    <tr>
+                        <td>E-Mail: </td>
+                        <td><input type="text" name="email" /></td>
+                    </tr>
+                    <tr>
+                        <td>Text: </td>
+                        <td><textarea name="text"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td>Absenden: </td>
+                        <td><input type="submit" name="Absenden" value="Absenden" /></td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+        
 </div>
+
+<style type="text/css">
+
+.comment_form {
+    color:black;
+}
+
+</style>
